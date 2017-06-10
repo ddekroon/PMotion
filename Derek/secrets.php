cd525e39dbd52dcdb4ff70e154cb5f2600da2cdd
ddekroon <ddekroon2@gmail.com>
2017-06-10 10:35:09 -0400

    Removing webapp folder from repository

--- Derek/secrets.php
+++ Derek/secrets.php
@@ -1,6 +1,3 @@
 <?php
-	$config['db']['host']   = "localhost"; //mysql.giantgoat.dreamhosters.com
-	$config['db']['user']   = "pmotiondata";
-	$config['db']['pass']   = "rememberHeavysalmon4";
-	$config['db']['dbname'] = "data_perpetualmotion";
+	
 ?>
