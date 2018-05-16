1b24b7b81ca3c605c786e858adb6b93fe36a70ef
nfroese <nick.froze@hotmail.com>
2018-05-16 18:41:59 -0400

    Undoing test

--- web-app/controllers/authcontroller.php
+++ web-app/controllers/authcontroller.php
@@ -214,7 +214,7 @@ class Controllers_AuthController extends Controllers_Controller {
 		$_SESSION[$this::SESSION_USER_ID] = $user->getId();
 	}
 	
-	function createNewAuthToken($user, $unhashedToken) {asdfasdf
+	function createNewAuthToken($user, $unhashedToken) {
 		
 		$selector = $this->generateRandomKey(12);
 		$hashedToken = $this->generateUserTokenHash($unhashedToken);
