5cb7e128eed34c2fc0e315f2036c3ed3c2fe9a4e
kconrad <kcomrade53@gmail.com>
2018-07-23 16:15:43 -0400

    Pushing changes - abandoning endeavour
    Dave doesn't want calendar integration directly from the website because he changes it semi-constantly and doesn't want issues with people downloading their schedule before changes are made

--- Kyle/KyleDev/download-ics.php
+++ Kyle/KyleDev/download-ics.php
@@ -2,13 +2,36 @@
 <?php 
 	date_default_timezone_set ("America/Toronto");
 
+	/* For accessing functions to get team and match info - remove the temp later when moved to allSports folder */
+	$temp = realpath($_SERVER['DOCUMENT_ROOT']).DIRECTORY_SEPARATOR.'allSports'.DIRECTORY_SEPARATOR;
+	require_once($temp.'includeFiles/tmpgFormFunctions2.php');
+
 	header('Content-type: text/calendar; charset=utf-8');
 	header('Content-Disposition: attachment; filename=pmotion-scheduled-matches.ics');
 	
 	include 'ics.php'; // Don't know if this is needed, or if semicolon is required either
 	echo Includes_ICS::header_to_string();
 
-	foreach(/*match*/) {
+	$data = getTeamsData($teamID, 0);
+
+	$teamObjs = $data['teamObjs'];
+	// $leagueID = $data['leagueID']; // Don't know if this is needed
+	$matchObj = getMatchData($teamObjs, $teamID);
+
+	if($matchObj != null) {
+		foreach($matchObj as $matchDay) {
+			foreach($matchDay as $match) {
+				$teamName = '';
+				$oppTeam = '';
+				$date = '';
+				$startTime = '';
+				$endTime = '';
+				if($sportID!=2) {$shirtColour = $matchObj->matchShirtColour};
+			}
+		}
+	}
+
+	foreach(/*match - find variable/class for this*/) {
 		$ics = new ics(array(
 			'location' => '',
 			'summary' => '',
@@ -24,7 +47,7 @@
 
 	echo Includes_ICS::footer_to_string();
 
-
+	/* ----------------------------- */ 
 	// All below is for web-app teams
 	foreach($teams as $curTeam) {
 		foreach($curTeam->getScheduledMatches() as $curMatch) {
@@ -46,7 +69,7 @@
 				'dtstart' => $startDateObj,
 				'dtend' => $endDateObj,
 				'url' => $basePath . $router->pathFor('schedule', ['leagueID' => $curTeam->getLeagueId()]),
-				'uid' => $curMatch->getId() . '@pmotionmatches', //so that our uid's don't match up with someone elses by accident. WILL NOT BE USED? - KYLE
+				'uid' => $curMatch->getId() . '@pmotionmatches', //so that our uid's don't match up with someone elses by accident. // WILL NOT BE USED? - KYLE
 				'description' => '' //TODO put link to venue in here.
 			));
 
