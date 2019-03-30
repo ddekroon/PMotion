9ffd7c80d95cc2e797276bff01bb395433f3bdd6
Derek Dekroon <ddekroon2@gmail.com>
2019-03-30 13:56:10 -0400

    Initial react app checkin

---
+++ react-app/src/web/routes/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Templates
+import TemplateNothing from '../components/TemplateNothing';
+import TemplateSidebar from '../components/TemplateSidebar';
+
+// Routes
+import Home from '../components/Home';
+
+import RecipesContainer from '../../containers/Recipes';
+import RecipesComponent from '../components/Recipes';
+import RecipeViewComponent from '../components/Recipe';
+
+import SignUpContainer from '../../containers/SignUp';
+import SignUpComponent from '../components/SignUp';
+
+import LoginContainer from '../../containers/Login';
+import LoginComponent from '../components/Login';
+
+import ForgotPasswordContainer from '../../containers/ForgotPassword';
+import ForgotPasswordComponent from '../components/ForgotPassword';
+
+import UpdateProfileContainer from '../../containers/UpdateProfile';
+import UpdateProfileComponent from '../components/UpdateProfile';
+
+import Error from '../components/Error';
+
+const Index = () => (
+  <Switch>
+    <Route
+      exact
+      path="/"
+      render={props => (
+        <TemplateSidebar>
+          <Home {...props} />
+        </TemplateSidebar>
+      )}
+    />
+    <Route
+      path="/sign-up"
+      render={props => (
+        <TemplateNothing pageTitle="Sign Up">
+          <SignUpContainer {...props} Layout={SignUpComponent} />
+        </TemplateNothing>
+      )}
+    />
+    <Route
+      path="/login"
+      render={props => (
+        <TemplateNothing pageTitle="Login">
+          <LoginContainer {...props} Layout={LoginComponent} />
+        </TemplateNothing>
+      )}
+    />
+    <Route
+      path="/forgot-password"
+      render={props => (
+        <TemplateNothing pageTitle="Forgot Password">
+          <ForgotPasswordContainer {...props} Layout={ForgotPasswordComponent} />
+        </TemplateNothing>
+      )}
+    />
+    <Route
+      path="/update-profile"
+      render={props => (
+        <TemplateSidebar pageTitle="Update Profile">
+          <UpdateProfileContainer {...props} Layout={UpdateProfileComponent} />
+        </TemplateSidebar>
+      )}
+    />
+    <Route
+      path="/recipes"
+      render={props => (
+        <TemplateSidebar pageTitle="Recipes">
+          <RecipesContainer {...props} Layout={RecipesComponent} />
+        </TemplateSidebar>
+      )}
+    />
+    <Route
+      path="/recipe/:id"
+      render={props => (
+        <TemplateSidebar pageTitle="Recipe View">
+          <RecipesContainer {...props} Layout={RecipeViewComponent} />
+        </TemplateSidebar>
+      )}
+    />
+    <Route
+      render={props => (
+        <TemplateSidebar pageTitle="404 - Page note found">
+          <Error {...props} title="404" content="Sorry, the route you requested does not exist" />
+        </TemplateSidebar>
+      )}
+    />
+  </Switch>
+);
+
+export default Index;
