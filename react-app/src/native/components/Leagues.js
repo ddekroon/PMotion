06de9c41a4eac2333bd828f6171748346597bbbc
Derek Dekroon <ddekroon2@gmail.com>
2019-06-18 00:10:56 -0400

    Passing formatted store data through to navigation screens via screenProps

--- react-app/src/native/components/Leagues.js
+++ react-app/src/native/components/Leagues.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createAppContainer, createMaterialTopTabNavigator} from 'react-navigation';
+import { createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';
 
 import FootballScreen from '../components/Football';
 import UltimateScreen from '../components/Ultimate';
@@ -7,13 +7,7 @@ import SoccerScreen from '../components/Soccer';
 import VolleyballScreen from '../components/Volleyball';
 import { Container } from 'native-base';
 
-/**
- * {lookups.sports.map((item, key)=>(
-         <Text key={key}> { item.name } </Text>)
-         )}
- */
-
-const appNavigator = createMaterialTopTabNavigator (
+const appNavigator = createMaterialTopTabNavigator(
   {
     Ultimate: UltimateScreen,
     VolleyBall: VolleyballScreen,
@@ -33,18 +27,74 @@ const appNavigator = createMaterialTopTabNavigator (
         borderBottomWidth: 3,
       },
       labelStyle: {
-        fontSize: 9 
+        fontSize: 9
       },
     }
   },
 );
 
-const AppIndex = createAppContainer(appNavigator);
+const AppIndex = createAppContainer(appNavigator)
 
 export default class Leagues extends React.Component {
-  render(){
-    return (
-      <AppIndex/> 
-    );
+  componentDidMount() {
+
+    var leaguesBySportAndSeason = [];
+    var component = this;
+
+    console.log("Props in mount function of leagues component");
+    //console.log(this.props.lookups.seasonsAvailableScoreReporter);
+    //console.log("Hi derek");
+
+    component.props.lookups.sports.forEach(function (curSport, index) {
+      var seasonsForSport = [];
+
+      component.props.lookups.seasonsAvailableScoreReporter.forEach(function (curSeason, index) {
+        var season = {
+          name: curSeason.name,
+          year: curSeason.year,
+          leagues: []
+        }
+
+        const curSeasonLeaguesBySport = curSeason.leagues.filter(league => league.sportId == curSport.id);
+        curSeasonLeaguesBySport.sort(function compare(a, b) {
+          if (a.dayNumber < b.dayNumber) {
+            return -1;
+          }
+
+          if (b.dayNumber < a.dayNumber) {
+            return 1;
+          }
+
+          return a.name < b.name ? -1 : 1;
+        });
+
+        season.leagues.push(curSeasonLeaguesBySport);
+        seasonsForSport.push(season);
+      });
+
+      leaguesBySportAndSeason.push(seasonsForSport);
+    });
+
+    component.setState(previousState => (
+      { leaguesBySport: leaguesBySportAndSeason }
+    ))
   }
-}   
+
+  //state object
+  state = {
+    isShowingText: true,
+    currentSport: 0, //Ultimate
+    leaguesBySport: []
+  }
+
+  render() {
+    return (
+      <AppIndex
+        screenProps={{ data: { seasons: this.state.leaguesBySport[this.state.currentSport], label: "Hi Derek" } }}
+        onNavigationStateChange={(prevState, currentState) => {
+          this.state.currentSport = currentState.index;
+        }}
+      />
+    )
+  }
+}
--- react-app/src/native/components/Ultimate.js
+++ react-app/src/native/components/Ultimate.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import {H1, Container, Content, Text} from 'native-base';
+import React from 'react'
+import { H1, Container, Content, Text } from 'native-base'
 
-export default class Ultimate extends React.Component{
-    render(){
-        return(
+export default class Ultimate extends React.Component {
+    render() {
+        console.log('this.props.screenProps.seasons in Ultimate', this.props.screenProps.data.seasons);
+        /* No idea why this isn't working.. getting an error on android saying basically seasons is undefined. Pretty junk.
+        const seasonsView = this.props.screenProps.data.seasons.map((curSeason) => {
+            var leagues = curSeason.leagues.map((league, leagueIndex) =>
+                <Text key={league.id}>{league.name}</Text>
+            )
+            return <Content><Text key={curSeason.name}>{curSeason.name} {curSeason.year}</Text>{leagues}</Content>;
+        });
+        console.log(seasonsView);*/
+
+        return (
             <Container>
                 <Content padder>
                     <H1>Ultimate Frisbee</H1>
+                    <Text>{this.props.screenProps.data.label}</Text>
+                    {<Text>{JSON.stringify(this.props.screenProps.data.seasons, null, 2)}</Text>}
+                    {/*seasonsView*/}
                 </Content>
-            </Container> 
+            </Container>
         );
     }
 }
