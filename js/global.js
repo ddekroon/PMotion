2ea1dcb9369003710905374adadb4165563f66f4
Derek Dekroon <ddekroon2@gmail.com>
2019-07-16 20:35:46 -0400

    Score reporter remembers name and email on submission, auto populates

--- js/global.js
+++ js/global.js
@@ -1,26 +1,26 @@
-function pmConfirm(message, true_func, false_func){
+function pmConfirm(message, true_func, false_func) {
     var container = document.createElement('div');
     //template for modal window
-    container.innerHTML += '<div class="modal fade custom-confirm">'+
-                        '<div class="modal-dialog">' +
-                            '<div class="modal-content">' +
-                                '<div class="modal-body" style="padding:30px 15px;">' +
-                                    '<div>' + message + '</div>' +
-                                '</div>' +
-                                '<div class="modal-footer">'+ 
-                                    '<button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-ok"></span> OK</button>' +
-                                    '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
-                                '</div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>';
+    container.innerHTML += '<div class="modal fade custom-confirm">' +
+        '<div class="modal-dialog">' +
+        '<div class="modal-content">' +
+        '<div class="modal-body" style="padding:30px 15px;">' +
+        '<div>' + message + '</div>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-ok"></span> OK</button>' +
+        '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
 
     //modal window
     var modal = container.firstChild;
 
     //get click OK button
     var ok = modal.getElementsByTagName('button')[0];
-    ok.onclick = function() {
+    ok.onclick = function () {
         $(modal).modal("hide");
         $(modal).detach();
         true_func();
@@ -28,7 +28,7 @@ function pmConfirm(message, true_func, false_func){
 
     //get click Cancel button
     var cancel = modal.getElementsByTagName('button')[1];
-    cancel.onclick = function() {
+    cancel.onclick = function () {
         false_func();
     }
 
@@ -41,18 +41,18 @@ function pmModal(url, id, title, buttons, onLoaded) {
 
     var container = document.createElement('div');
     //template for modal window
-    container.innerHTML += '<div class="modal fade pm-modal" id="' + id + '">'+
-                        '<div class="modal-dialog">' +
-                            '<div class="modal-content">' +
-                                '<div class="modal-header">' +
-                                    '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
-                                    '<h4 class="modal-title">' + title + '</h4>' +
-                                '</div>' +
-                                '<div class="modal-body"></div>' +
-                                '<div class="modal-footer"></div>' +
-                            '</div>' +
-                        '</div>' +
-                    '</div>';
+    container.innerHTML += '<div class="modal fade pm-modal" id="' + id + '">' +
+        '<div class="modal-dialog">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<button type="button" class="close" data-dismiss="modal">&times;</button>' +
+        '<h4 class="modal-title">' + title + '</h4>' +
+        '</div>' +
+        '<div class="modal-body"></div>' +
+        '<div class="modal-footer"></div>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
 
     //'<button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-ok"></span> OK</button>' +
     //'<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>' +
@@ -60,24 +60,47 @@ function pmModal(url, id, title, buttons, onLoaded) {
     var modal = container.firstChild;
     document.body.appendChild(modal);
 
-    $(modal).on('hidden.bs.modal', function() {
+    $(modal).on('hidden.bs.modal', function () {
         $(this).detach();
     });
 
-    $.each(buttons, function() {
+    $.each(buttons, function () {
         $(modal).find(".modal-footer").append(this);
     });
 
-    $.get(url, function(data) {
+    $.get(url, function (data) {
         $(modal).find(".modal-body").html(data);
 
-        if(typeof(onLoaded) === "function") {
+        if (typeof (onLoaded) === "function") {
             onLoaded($("#" + id));
 
             $(modal).modal();
         }
     }, "text")
-    .fail(function(response, textStatus, errorThrown) {
-        notify("Error connecting to server", "danger");
-    });
+        .fail(function (response, textStatus, errorThrown) {
+            notify("Error connecting to server", "danger");
+        });
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
